023e69e8e9f55c6706147239955ec8d455879d29
GodWeedZao <64776465+GodWeedZao@users.noreply.github.com>
2020-10-20 00:11:09 +0330

    Add files via upload

---
+++ src/GodWeedZao/MultiColorChat.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GodWeedZao;
+#============================================================#
+#                    Plugin By GodWeedZao                    #
+#============================================================#
+# ╔═╗╔═╗    ╔╗ ╔╗    ╔═══╗    ╔╗         ╔═══╗╔╗       ╔╗   #
+# ║║╚╝║║    ║║╔╝╚╗   ║╔═╗║    ║║         ║╔═╗║║║      ╔╝╚╗  #
+# ║╔╗╔╗║╔╗╔╗║║╚╗╔╝╔╗ ║║ ╚╝╔══╗║║ ╔══╗╔═╗ ║║ ╚╝║╚═╗╔══╗╚╗╔╝  #
+# ║║║║║║║║║║║║ ║║ ╠╣ ║║ ╔╗║╔╗║║║ ║╔╗║║╔╝ ║║ ╔╗║╔╗║╚ ╗║ ║║   #
+# ║║║║║║║╚╝║║╚╗║╚╗║║ ║╚═╝║║╚╝║║╚╗║╚╝║║║  ║╚═╝║║║║║║╚╝╚╗║╚╗  #
+# ╚╝╚╝╚╝╚══╝╚═╝╚═╝╚╝ ╚═══╝╚══╝╚═╝╚══╝╚╝  ╚═══╝╚╝╚╝╚═══╝╚═╝  #
+#============================================================#
+#    Report: https://Github.com/GodWeedZao/MultiColorChat    #
+#============================================================#
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class MultiColorChat extends PluginBase implements Listener
+{
+
+    public static $config;
+    public static $multiColor;
+
+    public function onEnable()
+    {
+        @mkdir($this->getDataFolder());
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        self::$config = $this->getConfig();
+        if (empty(self::$config->get('MultiColor'))) {
+            $this->getLogger()->error('We didn\'t find any color format for MultiColor Permission, its set to default!');
+            self::$multiColor = ['§l', '§a', '§2', '§c', '§4', '§e', '§6', '§d', '§5', '§b', '§3'];
+        } else {
+            self::$multiColor = self::$config->get('MultiColor');
+        }
+    }
+
+    /**
+     * @param $message
+     * @return string
+     */
+    public static function setMultiColor($message)
+    {
+        $color = "";
+        for ($numberPos = 0; $numberPos < strlen($message) - 1; $numberPos++) {
+            $color .= $message[$numberPos] . self::$multiColor[$numberPos % count(self::$multiColor)];
+        }
+        return $color .= $message[$numberPos];
+    }
+
+    /**
+     * @param PlayerChatEvent $event
+     */
+    public function onChat(PlayerChatEvent $event)
+    {
+        $player = $event->getPlayer();
+         if (($player->hasPermission('multi.color'))) {
+            $event->setMessage(self::setMultiColor($event->getMessage()));
+        }
+    }
+}
